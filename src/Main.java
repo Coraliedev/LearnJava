20074ac9317c4e754a07e8b36aaa20d25ca4a482
CoralieDev <coralie.acquaert@gmail.com>
2023-05-19 15:56:34 +0200

    java arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        for (Day moment : Day.values()) {
-            System.out.println("We are at " + moment.name().toLowerCase() + " and it's " + moment.getHour() + " o'clock" + " " + moment.getMessage());
-        }
+//        for (Day moment : Day.values()) {
+//            System.out.println("We are at " + moment.name().toLowerCase() + " and it's " + moment.getHour() + " o'clock" + " " + moment.getMessage());
+//        }
+//
+//        Apple apple = new Apple();
+//        apple.miam();
+//        apple.taste();
+//        PineApple pineApple = new PineApple();
+//        pineApple.miam();
+//        if (!(apple instanceof PeelFruit)) {
+//            System.out.println("Apple is not a PeelFruit");
+//        }
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        list.add("!");
 
-        Apple apple = new Apple();
-        apple.miam();
-        apple.taste();
-        PineApple pineApple = new PineApple();
-        pineApple.miam();
-        if (!(apple instanceof PeelFruit)) {
-            System.out.println("Apple is not a PeelFruit");
+        System.out.println(list);
+        if (list.contains("Hello")) {
+            System.out.println("Hello is in the list");
         }
+        list.remove(2);
+        System.out.println(list);
 
     }
 }
