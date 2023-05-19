e821ab78488de52341a6a3a4fe18364586ce76a6
CoralieDev <coralie.acquaert@gmail.com>
2023-05-19 16:14:38 +0200

    java hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -16,17 +19,29 @@ public class Main {
 //        if (!(apple instanceof PeelFruit)) {
 //            System.out.println("Apple is not a PeelFruit");
 //        }
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Hello");
-        list.add("World");
-        list.add("!");
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("Hello");
+//        list.add("World");
+//        list.add("!");
+//
+//        System.out.println(list);
+//        if (list.contains("Hello")) {
+//            System.out.println("Hello is in the list");
+//        }
+//        list.remove(2);
+//        System.out.println(list);
 
-        System.out.println(list);
-        if (list.contains("Hello")) {
-            System.out.println("Hello is in the list");
+        Map<String, Integer> ages = new HashMap<>();
+        ages.put("John", 25);
+        ages.put("Jane", 30);
+        ages.put("Jack", 20);
+        System.out.println(ages);
+        System.out.println(ages.get("Jane"));
+        ages.remove("Jane");
+        System.out.println(ages);
+        for(Entry<String, Integer> string : ages.entrySet()) {
+            System.out.println(string.getKey() + " " + string.getValue());
         }
-        list.remove(2);
-        System.out.println(list);
 
     }
 }
