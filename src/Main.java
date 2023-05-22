7feeffbdf1a9588febd4b122926c0fa56d9ffd6f
CoralieDev <coralie.acquaert@gmail.com>
2023-05-22 10:13:53 +0200

    java moving files

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,22 @@
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        File folder = new File("C:\\Users\\coral\\Desktop\\dossier1");
+        File source = new File("C:\\Users\\coral\\Desktop\\index.html");
+        File destination = new File("C:\\Users\\coral\\Desktop\\dossier1\\" + source.getName());
 
-        File file = new File("C:\\Users\\coral\\Desktop\\index.html");
-
-        if (!folder.exists()) {
-            boolean mkdir = folder.mkdir();
+        try {
+            Files.copy(source.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            FileWriter writer;
-            try {
-                writer = new FileWriter(file);
-                BufferedWriter bw = new BufferedWriter(writer);
-                bw.write("""
-                        <!DOCTYPE html>
-                        <html lang="fr">
-                        <head>
-                            <meta charset="UTF-8">
-                            <title>Document</title>
-                        </head>
-                        <body>
-                            <h1>Index</h1>
-                            </body>
-                        </html>
-                            
-                        """);
-                bw.close();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-                String line = reader.readLine();
-                while (line != null) {
-                    System.out.println(line);
-                    line = reader.readLine();
-                }
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        source.delete();
+
     }
 }
