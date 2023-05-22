1218f8d9ca5bb48576091b9290e1ed3dbf2b32f6
CoralieDev <coralie.acquaert@gmail.com>
2023-05-22 10:39:48 +0200

    java overloading

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        substract(5, 7, new CalculListener() {
-            @Override
-            public void onPositiveResult() {
-                System.out.println("Positive result");
-            }
-
-            @Override
-            public void onNegativeResult() {
-                System.out.println("Negative result");
-            }
-        });
-
+        Player player = new Player("John");
+        player.sendMessage();
     }
 
-    private static void substract(int a, int b, CalculListener listener) {
-        int result = a - b;
-        System.out.println("Result: " + result);
-        if (result > 0) {
-            listener.onPositiveResult();
-        } else {
-            listener.onNegativeResult();
-        }
-    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,39 +1,30 @@
 public class Player {
-    private String name;
-    private double health;
-    private double attack;
+    private final String name;
+    private final double health;
 
-    public Player(String name, double health, double attack) {
+
+    public Player(String name, double health) {
         this.name = name;
         this.health = health;
-        this.attack = attack;
     }
 
-    public void damage(double damage) {
-        this.health -= damage;
-        System.out.println(this.name + " took " + damage + " damage!");
+    public Player(String name) {
+        this(name, 500);
     }
+
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void sendMessage() {
+        sendMessage("We have " + health + " life points");
+    }
+
+    public void sendMessage(String message) {
+        System.out.println(name + " " + message);
     }
 
     public double getHealth() {
         return health;
-    }
-
-    public void setHealth(double health) {
-        this.health = health;
-    }
-
-    public double getAttack() {
-        return attack;
-    }
-
-    public void setAttack(double attack) {
-        this.attack = attack;
     }
 }
